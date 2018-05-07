431650248bb3a9d36240a1f56a4a148106b406be
Dan Kiss and Masen Matthews <masen@>
2018-05-07 15:55:57 -0700

    add CRUD functionality to reviews controller - not yet tested

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,42 @@
-class ReviewController < ApplicationController
+class ReviewsController < ApplicationController
 
+  def index
+    @destinations = Destination.all
+    json_response(@destinations)
+  end
 
+  def show
+    @destination = Destination.find(params[:id])
+    @review = Review.find(params[:id])
+  end
 
+  def create
+    @destination = Destination.find(params[:destination_id])
+    @review = @destination.destinations.new(review_params)
+    if @destination.save
+      flash[:notice] = "Destination successfully added!"
+      redirect_to destination_path(@destination)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @destination = Destination.find(params[:destination_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to destination_path(@destination)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @destination = Destination.find(params[:destination_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to destination_path(@destination)
+  end
 
   private
   def json_response(object, status = :ok)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :destinations
+  resources :destinations do
   resources :reviews
+  end
 end
